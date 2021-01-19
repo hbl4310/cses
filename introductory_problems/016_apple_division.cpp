554dd7d1d324e4ec4b00a97a3ca1d88908a5d682
hanbo <hanbo4310@gmail.com>
2021-01-19 15:08:13 +0000

    different approach: fill one side to close to halfmean

--- introductory_problems/016_apple_division.cpp
+++ introductory_problems/016_apple_division.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <sstream>
 #include <iterator>
+#include <cmath> 
 
 using namespace std; 
 
@@ -20,64 +21,58 @@ vector<string> split(const string &s, char delim) {
 }
 
 // https://cses.fi/problemset/task/1623
-void merge(int w[], int a, int c, int b) {
-    int n = b - a; 
-    int w2[(unsigned)n] = {};
-    int j = a, k = c; 
+float halfmean(int w[], int n) {
+    int s = 0; 
     for (int i = 0; i < n; i++) {
-        if ((j < c) && (k == b or w[j] <= w[k])) {
-            w2[i] = w[j]; 
-            j += 1; 
-        } else {
-            w2[i] = w[k]; 
-            k += 1; 
-        }
+        s += w[i];
     }
-    for (int i = 0; i < n; i++) {
-        w[a + i] = w2[i]; 
-    }
+    return (float)s / 2.0; 
 }
 
-void sort(int w[], int a, int b) {
-    int n = b - a; 
-    int c = 0; 
-    if (n > 1) {
-        c = a + n / 2; 
-        sort(w, a, c);
-        sort(w, c, b); 
-        merge(w, a, c, b);
+int fill(int w[], int i, int n, bool x[], int xsum, float m, int best) {
+    // cout << "i=" << i << ", x={"; 
+    // for (int j = 0; j < n; j++) {
+    //     cout << x[j] << " "; 
+    // }
+    // cout << "}, xsum=" << xsum << ", best=" << best << endl;
+    if (xsum + w[i] < m + 1) {
+        x[i] = 1; 
+        xsum += w[i]; 
+        if (xsum > m - 1) {
+            return xsum; 
+        }
     } 
+    if (i == n - 1) {
+        best = max(best, xsum); 
+        if (x[i] == 1) {
+            x[i] = 0; 
+            xsum -= w[i]; 
+        }
+        while (x[i] == 0 && i >= 0) {
+            i -= 1; 
+        }
+        if (i < 0) {
+            // cout << "exhausted" << endl; 
+            return best; 
+        }
+        x[i] = 0; 
+        xsum -= w[i]; 
+    }
+    return fill(w, i + 1, n, x, xsum, m, best); 
 }
 
 void solve(int w[], int n) {
-    int a[n] = {}, b[n] = {}; 
-    int ai = 0, bi = 0, aw = 0, bw = 0; 
-    sort(w, 0, n); 
-    // cout << "sorted w: "; 
-    // for (int i = 0; i < n; i++) {
-    //     cout << w[i] << " "; 
+    float m = halfmean(w, n); 
+    // cout << "half-mean: " << m << endl; 
+    bool x[(unsigned)n] = {}; 
+    int b = fill(w, 0, n, x, 0, m, 0) ; 
+    // cout << "best fill = " << b << endl; 
+    // cout << "x = {"; 
+    // for (int j = 0; j < n; j++) {
+    //     cout << x[j] << " "; 
     // }
-    for (int i = n - 1; i >= 0; i--) {
-        if (aw < bw) {
-            a[ai] = w[i]; 
-            ai += 1; 
-            aw += w[i]; 
-        } else {
-            b[bi] = w[i]; 
-            bi += 1; 
-            bw += w[i]; 
-        }
-    }
-    // cout << endl << "a: "; 
-    // for (int i = 0; i < ai; i++) {
-    //     cout << a[i] << " "; 
-    // }
-    // cout << endl << "b: "; 
-    // for (int i = 0; i < bi; i++) {
-    //     cout << b[i] << " "; 
-    // }
-    // cout << endl; 
-    cout << abs(aw - bw); 
+    // cout << "}" << endl; 
+    cout << round(2 * abs(b - m)); 
 }
 
 int main() {
